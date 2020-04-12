0358e06aab7a7afa1f5ea34f2ffd6a0370d33bf7
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-12 02:50:21 -0400

    add page 3 div

---
+++ nautilus_mockup-master/src/Components/Page3.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import clovercard from '../images/2-Article-page/clover_card.jpg';
+import './Page3.css'
+
+
+
+class Page3 extends Component{
+    render(){
+        return (
+            <div className='Article-grid-parent'>
+                <div className='Article-grid'>
+                <img src={clovercard} alt="clovercard"/> 
+                <div className='Article-page'>
+                    <h3>The Nautilus Spotlight</h3>
+                    <h2>Why The Laws Of Physics Are Inevitable</h2>
+                    <button className='Article-page-btn'>  
+                        Read Now</button>
+                </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default Page3;
