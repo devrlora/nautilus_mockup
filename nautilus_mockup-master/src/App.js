9bebfc458d17fab88408d21c6f5ef2d080b0c96b
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-11 23:40:52 -0400

    push app

---
+++ nautilus_mockup-master/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello World</p>
+      </header>
+      <body>
+        
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ nautilus_mockup-master/src/Images.js
@@ -0,0 +1,5 @@
+import Assets from "./Components/assets";
+
+
+
+// export default images;
