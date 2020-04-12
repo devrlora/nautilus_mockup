fad0a86dd1ef6c7c4c01bff9cabfbf493d0201a1
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-12 01:33:44 -0400

    create path to landing page png

--- nautilus_mockup-master/src/Components/Page1.js
+++ nautilus_mockup-master/src/Components/Page1.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import './assets';
+import matches from '../images/Landing-page/matches.png';
+
+
 
 class Page1 extends Component{
     render(){
         return (
             <div className='landing-grid'>
-<div>
-1
-</div>
-                
+                <div>
+                <img src={matches} alt="matches"/> 
+                </div>
+                <h1>hi</h1>
             </div>
         )
     }
-    
 }
 export default Page1;
