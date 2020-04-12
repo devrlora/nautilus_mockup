81845db8bb6d77635987a47de94a16013bfad60f
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-12 02:56:15 -0400

    import page 3

--- nautilus_mockup-master/src/App.js
+++ nautilus_mockup-master/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Page1 from './Components/Page1'
 import Page2 from './Components/Page2'
+import Page3 from './Components/Page3'
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
         <Page1 />
         <br/>
         <Page2 />
+        <br />
+        <Page3 />
       </body>
     </div>
   );
