f83111908f8b8bc8c4c72d431407e241b80710e1
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-12 00:12:25 -0400

    import assets landing page

---
+++ nautilus_mockup-master/src/Components/Page1.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './assets';
+
+class Page1 extends Component{
+    render(){
+        return (
+            <div className='landing-grid'>
+<div>
+1
+</div>
+                
+            </div>
+        )
+    }
+    
+}
+export default Page1;
