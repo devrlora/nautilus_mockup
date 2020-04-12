895f72a7300511f2b7f378c6d15bc390464d517d
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-12 01:32:57 -0400

    delete test text

--- nautilus_mockup-master/src/App.js
+++ nautilus_mockup-master/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
+import Page1 from './Components/Page1'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello World</p>
       </header>
       <body>
-        
+        <Page1 />
       </body>
     </div>
   );
