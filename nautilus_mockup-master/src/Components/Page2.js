0853ee024429c1928aaacb782c5faa04f6f0d285
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-12 02:23:23 -0400

    add second page divs

---
+++ nautilus_mockup-master/src/Components/Page2.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './Page2.css'
+
+
+
+class Page2 extends Component{
+    render(){
+        return (
+            <div className='nav-grid'>
+                <div className='navbar'>
+                    <ul>
+                        <li>Issues</li>
+                        <li>Topics</li>
+                        <li>Blog</li>
+                        <li>Newsletter</li>
+                        <li>Facebook Logo</li>
+                    </ul>
+                </div>
+                </div>
+        )
+    }
+}
+export default Page2;
